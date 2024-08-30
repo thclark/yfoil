62e201a817d7d9d089d28d1d1965c56c26ee2e50
Tom Clark <tom@octue.com>
2024-08-30 12:47:21 +0100

    STY: Update widths for a bit easier scientific formatting

--- tests/geometry_test.rs
+++ tests/geometry_test.rs
@@ -4,10 +4,6 @@ mod _utils;
 
 #[test]
 fn panic_on_erroneous_point() {
-    let result_or_error =
-        read_geometry_from_file("tests/fixtures/aerofoil_with_invalid_last_point.json");
-    assert_error!(
-        result_or_error,
-        InvalidGeometryError::BeginAndEndAtTrailingEdgeError()
-    );
+    let result_or_error = read_geometry_from_file("tests/fixtures/aerofoil_with_invalid_last_point.json");
+    assert_error!(result_or_error, InvalidGeometryError::BeginAndEndAtTrailingEdgeError());
 }
